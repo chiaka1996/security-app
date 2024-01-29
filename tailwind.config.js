210bfe6e798abadf1d6d07228946b86747b9e2d6
osuji chiaka <chiakajunior@yahoo.com>
2024-01-29 08:43:45 +0300

    created the login page

---
+++ tailwind.config.js
@@ -0,0 +1,58 @@
+tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            primary_color: '#3DC9B7',
+            background_color: "#162220",
+            basic: "#E7E8E7",
+            natural: "#FEFFFE",
+            placeholder_color: "#B4B5B4",
+            logo: "#C3EEE9"
+          },
+          padding: {
+            "1%": '1%',
+            "2%": "2%"
+          },
+          spacing: {
+                '5%': '5%',
+                '2%': '2%'
+            },
+          fontFamily: {
+            primary: ['Inter']
+          }
+        }
+      }
+    } 
+// module.exports = {
+//     theme: {
+//       screens: {
+//         sm: '480px',
+//         md: '768px',
+//         lg: '976px',
+//         xl: '1440px',
+//       },
+//       colors: {
+//         'blue': '#1fb6ff',
+//         'pink': '#ff49db',
+//         'orange': '#ff7849',
+//         'green': '#13ce66',
+//         'gray-dark': '#273444',
+//         'gray': '#8492a6',
+//         'gray-light': '#d3dce6',
+//         clifford: '#3DC9B7',
+//       },
+//       fontFamily: {
+//         sans: ['Graphik', 'sans-serif'],
+//         serif: ['Merriweather', 'serif'],
+//       },
+//       extend: {
+//         spacing: {
+//           '128': '32rem',
+//           '144': '36rem',
+//         },
+//         borderRadius: {
+//           '4xl': '2rem',
+//         }
+//       }
+//     }
+//   }
