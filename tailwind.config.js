e5f08c48d95cded70df53420bfab46ee6935173f
osuji chiaka <chiakajunior@yahoo.com>
2024-01-31 08:08:16 +0300

    completed the first dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,18 +7,50 @@ tailwind.config = {
             basic: "#E7E8E7",
             natural: "#FEFFFE",
             placeholder_color: "#B4B5B4",
-            logo: "#C3EEE9"
+            logo: "#C3EEE9",
+            site: "#6C0CE9",
+            tags: "#108C1E",
+            guards: "#C5A816",
+            green: "#00FF00",
+            table: "#EAECF0",
+            checkin: "#E8F9EA",
+            checkInText: '#0C6C17',
+            checkout: "#EDE4B7",
+            checkOutText: "#8C7710",
+            db: "#212D2B"
           },
           padding: {
             "1%": '1%',
-            "2%": "2%"
+            "2%": "2%",
+            "10%": "10%",
+            "5%": "5%",
+            "basic_padding": "5%",
+            'small': '2%',
+            "smaller": "1%",
+            "smallest": "0.5%"
           },
           spacing: {
                 '5%': '5%',
-                '2%': '2%'
+                '2%': '2%',
+                'mainMargin': "5%"
             },
           fontFamily: {
-            primary: ['Inter']
+            primary: ['Inter'],
+            headerWeight: "500",
+            normalWeight: "400",
+            biggerWeight: "700",
+          },
+          fontSize:{
+            big : "20px",
+            bigger: "30px",
+            normal: "14px",
+            small: "12px",
+            medium: "16px"
+          },
+          fontWeight:{
+            big: "500",
+            normal: "400",
+            bigger: "700",
           }
         }
       }
